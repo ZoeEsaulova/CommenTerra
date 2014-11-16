98b63548292fdcb881faef7b9469c2a1d20e7dfa
ZoeEsaulova <zoe.esaulova@gmail.com>
2014-11-16 11:03:31 +0100

    models updated

--- models/comments.js
+++ models/comments.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var CommentSchema = new mongoose.Schema({
   title: String,
   url: Url,
-  body: String,
+  text: String,
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   rating: {type: Number, default: 0},
   comment: { type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }
--- models/users.js
+++ models/users.js
@@ -5,15 +5,20 @@ var UserSchema = new mongoose.Schema({
 	  type: String, 
 	  unique: true,
 	  required: true,
-	  // trim removed empty spaces
+	  // trim removes empty spaces
 	  trim: true
-	  },
+  },
+  passwort: {
+	  type: String,
+	  required: true,
+	  trim: true	  
+  },
   email: {
 	  type: Email, 
 	  unique: true,
 	  required: true,
 	  trim: true
-	  },
+  },
   country: String,
   occupation: String,
   type: String,
@@ -21,11 +26,6 @@ var UserSchema = new mongoose.Schema({
   downvotes: {type: Number, default: 0},
   help: { Boolean, default: true },
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
-  passwort: {
-	  type: String,
-	  required: true,
-	  trim: true	  
-  }
 });
 
 mongoose.model('User', UserSchema);
