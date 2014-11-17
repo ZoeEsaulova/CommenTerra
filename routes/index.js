1e075710205507e8a311b579f78ac6f5e94fe002
ZoeEsaulova <ZoeEsaulova@users.noreply.github.com>
2014-11-17 14:47:25 +0100

    Update index.js
    Comments updated

--- routes/index.js
+++ routes/index.js
@@ -19,29 +19,39 @@ router.get('/newcomment', function(req, res) {
 /* POST to Add User Service */
 router.post('/adduser', function(req, res) {
 
-    // Get our form values. These rely on the "name" attributes
-    var userName = req.body.username;
-    var userEmail = req.body.useremail;
+    	// Get form values. These rely on the "name" attributes
+    	var userName = req.body.username;
+    	var userEmail = req.body.useremail;
 	var userPasswort = req.body.passwort;
+	//get model "User"
 	var User = require('mongoose').model('User');
+	//create new user document
 	var newUser = new User({ username: userName, email: userEmail, passwort: userPasswort });
+	
+	/*Save new user in databse. New users will be saved in "users" collection of the "ComTerDB"
+	You can check whether the user was saved using following commands in MongoDB shell:
+	use ComTerDB
+	db.users.find().pretty() */
 	newUser.save(function (err) {
 		if (err) return console.error(err);
 		console.log("Saved!!!");
 	});
-	console.log(userName);
-	console.log(userEmail);
-	console.log(userPasswort);
-	
 });
 
 /* POST to Add Comment Service */
 router.post('/addcomment', function(req, res) {
 	
-        // Get our form values. These rely on the "name" attributes
+        // Get form values. These rely on the "name" attributes
         var newTitle = req.body.title;
+        //get "Comment" model
 	var Comment = require('mongoose').model('Comment');
+	//create new comment document
 	var newComment = new Comment({ title: newTitle });
+	
+	/*Save new comment in databse. New comments will be saved in "comments" collection of the "ComTerDB"
+	You can check whether the user was saved using following commands in MongoDB shell:
+	use ComTerDB
+	db.comments.find().pretty() */
 	newComment.save(function (err) {
 		if (err) return console.error(err);
 		console.log("Comment Saved!!!");
