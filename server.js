e49f32599cc3ccf78cd8ac3af199bd0b0e0298ce
ZoeEsaulova <ZoeEsaulova@users.noreply.github.com>
2015-01-13 16:01:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -26,11 +26,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 
-/*
-app.engine('html', require('hogan-express'));
-app.set('view engine', 'html');
-*/
-
 //set static content
 app.use(express.static(__dirname + '/public'));
 app.use('/comments', express.static(__dirname + '/public'));
@@ -39,7 +34,6 @@ app.use('/comments/addtothread/:commentUrl/:commentId', express.static(__dirname
 app.use('/api/v1/search', express.static(__dirname + '/public'));
 app.use('/api/v1/search?', express.static(__dirname + '/public'));
 app.use('/profile', express.static(__dirname + '/public'));
-
 
 // required for passport (used for auth)
 app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
@@ -50,7 +44,7 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 // routes ======================================================================
 require('./app/routes/home.js')(app, passport); // load our routes and pass in our app and fully configured passport
 var comment = require('./app/routes/comment'),
-	profile = require('./app/routes/profile'),
+    profile = require('./app/routes/profile'),
     search = require('./app/routes/search');
 app.use('/comments', comment); // define route for /comments page
 app.use('/api/v1', search); //define route for search
